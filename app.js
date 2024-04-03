88167f9eb11158e3f5d1de7228d72993cd21b0b2
mike.osinowo <mikeson.jnr@gmail.com>
2024-04-03 12:33:45 +0100

    Setup requests and responses

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const feedRoutes = require('./routes/feed');
 
 const app = express();
+
+// Content-Type: application/json
+app.use(bodyParser.json());
 
 // Forward any incoming requests that starts with /feed
 app.use('/feed', feedRoutes);
