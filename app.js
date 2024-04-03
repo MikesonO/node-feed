f4aab26ded52fcf7403dc4a7d8a8cdf016534cde
mike.osinowo <mikeson.jnr@gmail.com>
2024-04-03 16:58:06 +0100

    Setup static images & error handling

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv').config();
@@ -11,6 +13,7 @@ const app = express();
 
 // Content-Type: application/json
 app.use(bodyParser.json());
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 // Resolve CORS error
 app.use((req, res, next) => {
@@ -22,6 +25,15 @@ app.use((req, res, next) => {
 
 // Forward any incoming requests that starts with /feed
 app.use('/feed', feedRoutes);
+
+
+// Error Handling
+app.use((error, req, res, next) => {
+    console.log(error);
+    const status = error.statusCode || 500;
+    const message = error.message;
+    res.status(status).json({ message: message });
+});
 
 mongoose.connect(MONGODB_URI)
     .then(result => {
--- controllers/feed.js
+++ controllers/feed.js
@@ -23,10 +23,9 @@ exports.createPost = (req, res, next) => {
 
     // If errors exist
     if (!errors.isEmpty()) {
-        return res.status(422).json({
-            message: "Validation failed, entered data is incorrect.",
-            errors: errors.array()
-        })
+        const error = new Error("Validation failed, entered data is incorrect.");
+        error.statusCode = 422;
+        throw error;
     }
 
     const title = req.body.title;
@@ -51,6 +50,9 @@ exports.createPost = (req, res, next) => {
             });
         })
         .catch(err => {
-            console.log(err);
-        })
-}
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
+        });
+};
