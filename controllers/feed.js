7854be4c15340cebba04167a244bfb4830a8111a
mike.osinowo <mikeson.jnr@gmail.com>
2024-04-03 16:23:35 +0100

    Store post data in mongoDB

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,5 +1,7 @@
 const { validationResult } = require('express-validator');
 
+const Post = require('../models/post');
+
 exports.getPosts = (req, res, next) => {
     res.status(200).json({
         posts: [{
@@ -18,29 +20,37 @@ exports.getPosts = (req, res, next) => {
 
 exports.createPost = (req, res, next) => {
     const errors = validationResult(req);
-    const title = req.body.title;
-    const content = req.body.content;
 
     // If errors exist
-    if (!error.isEmpty()) {
+    if (!errors.isEmpty()) {
         return res.status(422).json({
             message: "Validation failed, entered data is incorrect.",
             errors: errors.array()
         })
     }
 
-    // Create Posts in DB
-    res.status(201).json({
-        message: "Post created successfully!",
-        post: {
-            _id: new Date().toISOString,
-            title: title,
-            content: content,
-            creator: {
-                name: "Mikeson"
-            },
-            createdAt: new Date()
+    const title = req.body.title;
+    const content = req.body.content;
+
+    // Post data
+    const post = new Post({
+        title: title,
+        imageUrl: 'images/book.png',
+        content: content,
+        creator: {
+            name: "Mikeson"
         }
     })
-
+    // Save / Store post data to DB
+    post.save()
+        .then(result => {
+            console.log(result);
+            res.status(201).json({
+                message: "Post created successfully!",
+                post: result
+            });
+        })
+        .catch(err => {
+            console.log(err);
+        })
 }
