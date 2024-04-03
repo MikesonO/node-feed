d84e2f1c15aca43f80a631007da5baa49544ef12
mike.osinowo <mikeson.jnr@gmail.com>
2024-04-03 12:52:01 +0100

    Resolve CORS error

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ const app = express();
 // Content-Type: application/json
 app.use(bodyParser.json());
 
+// Resolve CORS error
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*'); // '*' allows access to any client OR 'codepen.io' for specific access.
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE'); // Methods to allow clients
+    res.setHeader('Access-Control-Allow-Headers', ' Content-Type, Authorization');
+    next();
+})
+
 // Forward any incoming requests that starts with /feed
 app.use('/feed', feedRoutes);
 
