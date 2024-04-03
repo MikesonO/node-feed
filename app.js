4ac8b17e579ca59aa99d528b9e2ab0827dc86463
mike.osinowo <mikeson.jnr@gmail.com>
2024-04-03 12:08:37 +0100

    Route setup

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require('express');
 
+const feedRoutes = require('./routes/feed');
+
 const app = express();
+
+// Forward any incoming requests that starts with /feed
+app.use('/feed', feedRoutes);
 
 app.listen(8080);
