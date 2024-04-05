0d8776c89f9162abc281a2af7a7314d43a969ff2
mike.osinowo <mikeson.jnr@gmail.com>
2024-04-05 11:25:29 +0100

    Add imageUrl to createPost

--- controllers/feed.js
+++ controllers/feed.js
@@ -48,13 +48,20 @@ exports.createPost = (req, res, next) => {
         throw error;
     }
 
+    if (!req.file) {
+        const error = new Error('No image provided.');
+        error.statusCode = 422;
+        throw error;
+    }
+
     const title = req.body.title;
+    const imageUrl = req.file.path;
     const content = req.body.content;
 
     // Post data
     const post = new Post({
         title: title,
-        imageUrl: 'images/book.png',
+        imageUrl: imageUrl,
         content: content,
         creator: {
             name: "Mikeson"
