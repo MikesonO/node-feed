852b71ce844ad8ee72c402ebf98cb87020c62161
mike.osinowo <mikeson.jnr@gmail.com>
2024-04-17 10:47:47 +0100

    Add fetch posts functionality

--- graphql/schema.js
+++ graphql/schema.js
@@ -26,6 +26,11 @@ type AuthData {
     userId: String!
 }
 
+type PostData {
+    posts: [Post!]!
+    totalPosts: Int!
+}
+
 input UserInputData {
     email: String!
     name: String!
@@ -41,6 +46,7 @@ input PostInputData {
 
 type RootQuery {
     login(email: String!, password: String!): AuthData!
+    posts: PostData!
 }
 
 type RootMutation {
